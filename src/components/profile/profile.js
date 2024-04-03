7ea4e1c90fc3114b9d2cfc142fe1a32da9d1b04d
Raman Saini <120019686+saini-saini@users.noreply.github.com>
2024-04-03 12:39:11 +0530

    profile section changes

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import Avatar from '@mui/material/Avatar';
 import './profile.css';
@@ -6,22 +6,35 @@ import { Formik, Form, Field, ErrorMessage } from 'formik';
 import TextError from './errorMessage';
 // import { useNavigate } from 'react-router-dom';
 import { ProfileValidation } from '../formValidation/formValidation';
+import EditIcon from '@mui/icons-material/Edit';
 
 const Profile = () => {
-
-    // const navigate = useNavigate();
     const signUpUserData = useSelector(state => state.userDetail.loggedInUser);
-    // const onSubmit = (values) => {
-    //     navigate("/layout");
-    // };
+    const [disabled, setDisabled] = useState(true);
 
+    const editClick = () => {
+        setDisabled(!disabled);
+
+    }
+    
     return (
         <div className='profile-wrapper'>
-            <h1 style={{ color: "white", backgroundColor: "#282828", margin: "0px", position: "absolute", top: "23px", left: "50%" }}>Profile</h1>
+            <div style={{
+                display: "flex",
+                position: "absolute",
+                top: "11px",
+                left: "50%",
+                justifyContent: "center",
+                alignItems: "center",
+                gap: "15px"
+            }}>
+                <h1 style={{ color: "white", backgroundColor: "#282828", margin: "0px" }}>Profile</h1>
+                <EditIcon sx={{ color: "white", cursor: "pointer" }} onClick={() => editClick()} />
+            </div>
             <div className='profile-container'>
                 <div className='profile-img'>
-                    <Avatar sx={{ bgcolor: "#282828", cursor: "pointer", width: "170px", height: "170px", boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px" }}>
-                        {signUpUserData && signUpUserData.username ? signUpUserData?.username.charAt(0).toUpperCase() : ''}
+                    <Avatar sx={{ bgcolor: "#282828", cursor: "pointer", width: "170px", height: "170px", boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px", borderRadius: "5%" }}>
+                        <h1>{signUpUserData && signUpUserData.username ? signUpUserData?.username.charAt(0).toUpperCase() : ''}</h1>
                     </Avatar>
                 </div>
                 <Formik
@@ -37,22 +50,22 @@ const Profile = () => {
                             <div className='form-input'>
                                 <div className='form-input-wrapper'>
                                     <label htmlFor="">Name</label>
-                                    <Field type="text" name="username" placeholder="username" className="input" autoComplete="off" disabled />
+                                    <Field type="text" name="username" placeholder="username" className="input" autoComplete="off" disabled={disabled} style={{ backgroundColor: disabled ? '#3E3E3E' : '#777777' }} />
                                     <ErrorMessage name="username" component={TextError} />
                                 </div>
                                 <div className='form-input-wrapper'>
                                     <label htmlFor="">Email</label>
-                                    <Field type="text" name="email" placeholder="email address" className="input" autoComplete="off" disabled />
+                                    <Field type="text" name="email" placeholder="email address" className="input" autoComplete="off" disabled={disabled} style={{ backgroundColor: disabled ? '#3E3E3E' : '#777777' }} />
                                     <ErrorMessage name="email" component={TextError} />
                                 </div>
                                 <div className='form-input-wrapper'>
                                     <label htmlFor="">Number</label>
-                                    <Field type="text" name="number" placeholder="contact number" className="input" autoComplete="off" disabled />
+                                    <Field type="text" name="number" placeholder="contact number" className="input" autoComplete="off" disabled={disabled} style={{ backgroundColor: disabled ? '#3E3E3E' : '#777777' }} />
                                     <ErrorMessage name="number" component={TextError} />
                                 </div>
-                                {/* <div>
-                                    <button className='update-btn' type="submit" >Save</button>
-                                </div> */}
+                                <div>
+                                    <button className='update-btn' type="submit" disabled={disabled} style={{ display: disabled ? 'none' : 'block' }} onClick={() => console.log("save clicked")}>Save</button>
+                                </div>
                             </div>
                         </Form>
                     </div>
